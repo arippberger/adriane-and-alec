85afb0fdc58606f6ead993b5d3e6e5c06a0e8adf
Alec Rippberger <alec@westwerk.com>
2014-11-30 17:33:18 -0600

    initial commit

---
+++ wp-content/themes/roots-sass-master/templates/content-home.php
@@ -0,0 +1,19 @@
+<div class="wrap container-fluid full-width" role="document">
+    <div class="content row">
+    	<!-- <main class="main" role="main"> -->
+			<div class="row">
+				<div class="intro-image">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/aa-steps.jpg" alt="...">
+				</div>
+			</div>
+		<!-- </main> -->
+	</div>
+</div>
+<div class="wrap container">
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			<?php the_content(); ?>
+		</div>
+	</div>
+</div>
+<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
---
+++ wp-content/themes/roots-sass-master/templates/footer.php
@@ -0,0 +1,10 @@
+<footer class="content-info" role="contentinfo">
+  <div class="container">
+    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="static-footer col-sm-4 col-sm-offset-4">
+    	<div class="inner-wrap">
+    		<h3>Made with love by Adriane &amp; Alec</h3>
+    	</div>
+    </div>
+  </div>
+</footer>
